bec191e4362039c8fb85cec1e06d7894f895773f
MINH DUY <duy9803@gmail.com>
2023-07-11 20:55:04 +0700

    First commit

---
+++ src/main/java/com/fpoly/VietFastFoodApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietFastFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietFastFoodApplication.class, args);
+	}
+
+}
